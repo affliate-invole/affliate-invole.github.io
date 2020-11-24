7cc967d5c0e6f295804f1642a9b634f23e05ea1f
azhimeblog <azhime.blog@gmail.com>
2020-11-24 17:44:33 +0700

    commit unload js

--- timer.js
+++ timer.js
@@ -21,3 +21,15 @@ window.onload = function() {
         display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
 };
+
+function unloadAllJS() {
+    var jsArray = new Array();
+    jsArray = document.getElementsByTagName('script');
+    for (i = 0; i < jsArray.length; i++) {
+        if (jsArray[i].id) {
+            unloadJS(jsArray[i].id)
+        } else {
+            jsArray[i].parentNode.removeChild(jsArray[i]);
+        }
+    }
+};
