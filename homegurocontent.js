7757649b4b8278af13da333045fc64711742e08a
azhimeblog <azhime.blog@gmail.com>
2020-11-24 16:05:48 +0700

    update homeoguru

--- homegurocontent.js
+++ homegurocontent.js
@@ -3,14 +3,34 @@ data = 'https://homeguru.homepro.co.th/wp-json/wp/v2/posts/';
 fetch(data)
     .then(res => res.json())
     .then((result) => {
-        //console.log(result[0])
 
-        for (i = 0; i < 2; i++) {
+        for (i = 0; i < 3; i++) {
 
-            console.log(result[i]);
-            document.getElementById("homegurucontent").innerHTML += "<p>" + result[i].title.rendered + "</p>";
+            console.log(result[i].title.rendered);
+            console.log(result[i].featured_media);
+            var x = result[i].featured_media;
+
+            document.getElementById("articlehomeguru").innerHTML += "<div class='article'><p><b>" + result[i].title.rendered + "</b></p><div class = 'article-body'><p> " + result[i].content.rendered.slice(0, 180) + "..<a href = '" + result[i].link + "'' > <br>อ่านบทความต่อ </a></p ></div></div>";
 
         }
 
     })
     .catch(err => { throw err });
+
+
+
+
+function Imgwp(imgid) {
+    var img, k, p = "";
+    img = 'https://homeguru.homepro.co.th/wp-json/wp/v2/media/' + imgid;
+    fetch(img)
+        .then(res => res.json())
+        .then((img) => {
+
+            if (img.id = x)
+                console.log(img.guid.rendered);
+        })
+        .catch(err => { throw err });
+}
+
+Imgwp(18386);
--- productall.js
+++ productall.js
@@ -5,10 +5,10 @@ function loadproduct(namecat) {
         .then(res => res.json())
         .then((result) => {
 
-            for (i = Math.floor(Math.random() * Math.floor(2)); i < 50000; i++) {
+            for (i = Math.floor(Math.random() * Math.floor(50)); i < 50000; i++) {
                 if (result[i].name_th == namecat) {
 
-                    document.getElementById("productname").innerHTML += "<div class='card cardmain'>" + "<div class='row cardproduct'>ผ่อน 0% นาน 10 เดือน </div>" + "<a href=' "+ result[i].url  +"'><img src='" + result[i].hp_image_medium + " 'class='card-img-top' alt='" + result[i].hp_full_desc_th + "' " + result[i].hp_full_desc_th + " ' loading='lazy'></a></img><div class='card-body'><p class='sku'> SKU: " + result[i].partnumber + "</p></p><p class='productname'> " + result[i].hp_full_desc_th + "</p> <p> ราคาล่าสุด " + result[i].price + "</p>" + " <a href='" + result[i].url + "'><p class='btnproduct'><button type='button' class='btn btn-primary'>ดูราคาล่าสุด</button></p></a>";
+                    document.getElementById("productname").innerHTML += "<div class='card cardmain productcard '>" + "<div class='row cardproduct'>ผ่อน 0% นาน 10 เดือน </div>" + "<a href=' " + result[i].url + "'><img src='" + result[i].hp_image_medium + " 'class='card-img-top' alt='" + result[i].hp_full_desc_th + "' " + result[i].hp_full_desc_th + " ' loading='lazy'></a></img><div class='card-body'><p class='sku'> SKU: " + result[i].partnumber + "</p></p><p class='productname'> " + result[i].hp_full_desc_th + "</p> <p> ราคาล่าสุด " + result[i].price + "</p>" + " <a href='" + result[i].url + "'><p class='btnproduct'><button type='button' class='btn btn-primary'>ดูราคาล่าสุด</button></p></a>";
                 }
             }
 
