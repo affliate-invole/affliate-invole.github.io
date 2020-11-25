da4bf22f2dad14f9adca3a2e3b8d0ae1e89460ee
azhimeblog <azhime.blog@gmail.com>
2020-11-25 13:28:58 +0700

    update wordpress api
    ดึงข้อมูลจาก Homeguru ไปแสดง

--- homegurocontent.js
+++ homegurocontent.js
@@ -1,16 +1,16 @@
 var data, i, x = "";
-data = 'https://homeguru.homepro.co.th/wp-json/wp/v2/posts/';
+data = 'https://homeguru.homepro.co.th/wp-json/wp/v2/posts/?_embed';
 fetch(data)
     .then(res => res.json())
     .then((result) => {
 
-        for (i = 0; i < 3; i++) {
+        for (i = 0; i < 6; i++) {
 
-            console.log(result[i].title.rendered);
-            console.log(result[i].featured_media);
-            var x = result[i].featured_media;
+            // console.log(result[i].title.rendered);
+            // console.log(result[i].featured_media);
+            // console.log(result[i]._embedded['wp:featuredmedia']['0'].source_url);
 
-            document.getElementById("articlehomeguru").innerHTML += "<div class='article'><p><b>" + result[i].title.rendered + "</b></p><div class = 'article-body'><p> " + result[i].content.rendered.slice(0, 180) + "..<a href = '" + result[i].link + "'' > <br>อ่านบทความต่อ </a></p ></div></div>";
+            document.getElementById("articlehomeguru").innerHTML += "<div class='article'><p><b>" + result[i].title.rendered + "</b><a href='" + result[i].link + " '><img class='articleimg' src='" + result[i]._embedded['wp:featuredmedia']['0'].source_url + "'></a></p><div class = 'article-body'><p> " + result[i].content.rendered.slice(0, 180) + "..<a href = '" + result[i].link + "' > <br> อ่านบทความต่อ </a></p ></div></div>";
 
         }
 
@@ -32,5 +32,3 @@ function Imgwp(imgid) {
         })
         .catch(err => { throw err });
 }
-
-Imgwp(18386);
