430d96343f8e02d5ba5c3c769628f47a09fe1135
azhimeblog <azhime.blog@gmail.com>
2020-11-26 15:27:09 +0700

    commit producr and and scripts

--- productall.js
+++ productall.js
@@ -1,11 +1,19 @@
-eval(function(p, a, c, k, e, d) {
-    e = function(c) { return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36)) };
-    if (!''.replace(/^/, String)) {
-        while (c--) { d[e(c)] = k[c] || e(c) }
-        k = [function(e) { return d[e] }];
-        e = function() { return '\\w+' };
-        c = 1
-    };
-    while (c--) { if (k[c]) { p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]) } }
-    return p
-}('f g(5){F h,i,x="";h=\'E/D.u\';G(h).s(m=>m.u()).s((1)=>{H(i=d.r(d.C()*d.r(J));i<I;i++){L(1[i].y==5){4.7("9").e+="<3 2=\'j A w \'>"+"<3 2=\'v B\'>ผ่อน 0% นาน 10 เดือน </3>"+"<a o=\' "+1[i].k+"\'><6 n=\'"+1[i].z+" \'2=\'j-6-K\' W=\'"+1[i].c+"\' "+1[i].c+" \' 8=\'12\'></a></6><3 2=\'j-11\'><p 2=\'Z\'> 14: "+1[i].13+"</p></p><p 2=\'9\'> "+1[i].c+"</p> <p> ราคาล่าสุด "+1[i].M+"</p>"+" <a o=\'"+1[i].k+"\'><p 2=\'15\'><b Y=\'b\' 2=\'l l-X\'>เลือกซื้อสินค้า</b></p></a>"}}}).Q(t=>{P t})}g("เครื่องฟอกอากาศ");f q(){4.7("9").e="";4.7("8").e="<6 n=\'O.N\'>";R(4.7("8").S.V="U",T)}f 8(5){g(5);q()}', 62, 68, '|result|class|div|document|namecat|img|getElementById|loading|productname||button|hp_full_desc_th|Math|innerHTML|function|loadproduct|datasa||card|url|btn|res|src|href||delproduct|floor|then|err|json|row|productcard||name_th|hp_image_medium|cardmain|cardproduct|random|productfeed|jsondata|var|fetch|for|50000|50|top|if|price|gif|heart|throw|catch|setTimeout|style|2000|none|display|alt|primary|type|sku||body|lazy|partnumber|SKU|btnproduct'.split('|'), 0, {}))
+function loadproduct(a) {
+    var b, i, x = "";
+    b = 'jsondata/homeproallproductcsv.json';
+    fetch(b).then(res => res.json()).then((result) => {
+        swal("โหลดข้อมูลสินค้าสำเร็จ!", "ข้อมูลสินค้า  " + a + "  จะแสดงด้านล่าง", "success");
+        for (i = Math.floor(Math.random() * Math.floor(50)); i < 50000; i++) { if (result[i].name_th == a) { document.getElementById("productname").innerHTML += "<div class='card cardmain productcard '>" + "<div class='row cardproduct'>ผ่อน 0% นาน 10 เดือน </div>" + "<a href=' " + result[i].url + "'><img src='" + result[i].hp_image_medium + " 'class='card-img-top' alt='" + result[i].hp_full_desc_th + "' " + result[i].hp_full_desc_th + " ' loading='lazy'></a></img><div class='card-body'><p class='sku'> SKU: " + result[i].partnumber + "</p></p><p class='productname'> " + result[i].hp_full_desc_th + "</p> <p> ราคาล่าสุด " + result[i].price + "</p>" + " <a href='" + result[i].url + "'><p class='btnproduct'><button type='button' class='btn btn-primary'>เลือกซื้อสินค้า</button></p></a>" } }
+    }).catch(err => { throw err })
+}
+
+function delproduct() {
+    document.getElementById("productname").innerHTML = "";
+    document.getElementById("loading").innerHTML = "<img src='heart.gif'>";
+    setTimeout(document.getElementById("loading").style.display = "none", 2000)
+}
+
+function loading(a) {
+    loadproduct(a);
+    delproduct();
+}
